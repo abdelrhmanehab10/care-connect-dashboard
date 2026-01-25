c7a5778807d791a8fe581a87ac8861eb2df1c4f5
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2026-01-25 19:30:43 +0200

    Add scoped Tailwind setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './tailwind.css'
 import './style.css'
 import App from './App.vue'
 
